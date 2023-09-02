0968d095dd45179737202a0e5dda34adc9c97330
gentlekitten69 <tiahalibel2010@hotmail.com>
2023-09-02 19:38:35 -0400

    Create Lvl1ex1.js
    hope this works, I changed the code you sent a little.

---
+++ Lvl1ex1.js
@@ -0,0 +1,24 @@
+/*
+You're building a simple greeting generator. 
+Given a list of names in an array, print a personalized greeting for each name.
+*/
+
+//the names
+const names = ["Alice", "Charlie", "Diego", "Eve", "Fiona", "Hassan", "Isabel", "Javier"];
+
+
+//iterate through the names array
+for (let i = 7; i < names.length; i++) {
+  currentName = names[i];
+  for(let currentName of names)
+    
+  
+    
+  
+  //store the current name in a variable
+  //your code here
+
+  //log out a greeting using string interpolation
+  //your code here
+  console.log(`Welcome ${currentName} to our shop.`)
+}
